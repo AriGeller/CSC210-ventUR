f62a2df36507647712c850ee87e1146a247b8dc7
Unknown <ageller2@u.rochester.edu>
2016-10-08 18:05:57 -0400

    Change

--- scripts/LogIn.js
+++ scripts/LogIn.js
@@ -14,15 +14,8 @@ function setup() {
 	document.getElementById("Log In").onclick = function() {
 		console.log("You clicked log in.")
 		var username = $('#Username').val();
-		if (username == "") {
-            alert("Please enter a valid username.")
-        } else {
-            console.log(username);
-        }
-        var password = $('#Password').val();
-        if (password.length < 4 || password.length > 15) {
-            alert("Please enter a valid password. A valid password has between 4 and 15 characters.")
-        }
+		
+        
 	};
 
 	document.getElementById("Sign Up").onclick = function() {
