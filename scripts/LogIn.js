630186945ba9b81ef24564a76f23ebddb50b0256
Unknown <ageller2@u.rochester.edu>
2016-10-08 17:42:54 -0400

    Add buttons and connect them to JS file

--- scripts/LogIn.js
+++ scripts/LogIn.js
@@ -5,4 +5,17 @@
 
 $(document).ready(function() {
 	console.log("Script loaded...");
+	setup();
 });
+
+function setup() {
+
+
+	document.getElementById("Log In").onclick = function() {
+		console.log("You clicked log in.")
+	};
+
+	document.getElementById("Sign Up").onclick = function() {
+		console.log("You clicked sign up");
+	};
+}
