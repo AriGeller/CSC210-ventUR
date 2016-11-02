29b098231a648f148aafb07599616e74bbf13624
Sam Wlody <SWlody@gmail.com>
2016-11-01 20:39:22 -0400

    mend
    Almost done with friends code for db - need help with json

--- cgi-bin/getfriends.py
+++ cgi-bin/getfriends.py
@@ -5,7 +5,9 @@ import sqlite3
 conn = sqlite3.connect('users.db')
 c = conn.cursor()
 
-c.execute('SELECT friends FROM %s_friends WHERE status = "accepted"', username)
+status = #I think this should be "accepted"
+
+c.execute('SELECT friends FROM ?_friends WHERE status = ?', (username, status))
 
 friends = c.fetch()
 
