e14291e54d0c82b6c7f018b4f91c98addad8f2b5
Sam Wlody <SWlody@gmail.com>
2016-11-03 17:55:32 -0400

    GetFriends

--- cgi-bin/getfriends.py
+++ cgi-bin/getfriends.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
 import sqlite3
+import json
+import cgi
+
+form = cgi.FieldStorage()
 
 conn = sqlite3.connect('users.db')
 c = conn.cursor()
 
+username = form['username'].value
 status = "accepted"
 
 c.execute('SELECT friends FROM ?_friends WHERE status = ?', (username, status))
 
 friends = c.fetch()
 
-#return friends as json
+print json.dumps(friends)
