f553daa5041c1dd3c55fabcb92b7a92089f3b88c
Sam Wlody <SWlody@gmail.com>
2016-11-03 18:51:35 -0400

    Small fix

--- cgi-bin/addfriend.py
+++ cgi-bin/addfriend.py
@@ -17,10 +17,11 @@ friend = form['friend'].value
 c.execute('SELECT status FROM ?_friends WHERE username = ?', (username, friend))
 status = c.fetchone()[0]
 
-
-if status is None:
+try:
+	status
+except NotAdded:
     c.execute('INSERT INTO ?_friends VALUES (?, ?)', (username, friend, "pending"))
     c.execute('INSERT INTO ?_friends VALUES (?, ?)', (friend, username, "requested"))
-elif status == "requested":
+else:
     c.execute('UPDATE ?_friends SET status = "accepted" WHERE username = ?', (friend, username))
     c.execute('UPDATE ?_friends SET status = "accepted" WHERE username = ?', (username, friend))
