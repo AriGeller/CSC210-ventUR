0a73b2def883c0b11de48c6de8a8b798c13cc14d
Sam Wlody <SWlody@gmail.com>
2016-11-03 21:55:46 -0400

    fix???

--- cgi-bin/addfriend.py
+++ cgi-bin/addfriend.py
@@ -14,18 +14,18 @@ c = conn.cursor()
 username = form['username'].value
 friend = form['friend'].value
 
-sql = "SELECT status FROM %s_friends WHERE username = ?" % friend
-c.execute(sql, [username])
-status = c.fetchone()[0]
-
-try:
-	status
-except NotAdded:
+sql = "SHOW TABLES LIKE %s_friends" % username
+c.execute(sql)
+result = c.fetchone()[0]
+if result:
 	sql = "INSERT INTO %s_friends VALUES (?, ?)" % username
     c.execute(sql, (friend, "pending"))
     sql = "INSERT INTO %s_friends VALUES (?, ?)" % friend
     c.execute(sql, (username, "requested"))
 else:
+	sql = "SELECT status FROM %s_friends WHERE username = ?" % username
+	c.execute(sql, [friend])
+	status = c.fetchone()[0]
 	sql = "UPDATE %s_friends SET status = accepted WHERE username = ?" % friend
     c.execute(sql, [username])
     sql = "UPDATE %s_friends SET status = accepted WHERE username = ?" % username
